766424983131099474288448aba69fe3926d54ba
Locleabc <locleabc98@gmail.com>
2025-04-27 09:41:42 +0700

    update

---
+++ data/products.js
@@ -0,0 +1,45 @@
+var list_products = [{
+    "name": "Pin EVE 25P",
+    "company": "EVE",
+    "img": "/img/EVE25P.jpg",
+    "price": "28.000",
+    "star": 4.9,
+    "rateCount": 16442,
+    "promo": {
+        "name": "giamgia",
+        "value": "1.000"
+    },
+    "detail": {
+        "Capacity": "2500 mAh",
+        "Cycle": "800 - 1000 lần",
+        "MaxDisCharge": "30A",
+        "MaxCharge": "5A",
+        "Color": "Xanh lá",
+        "Size": "18 x 65 mm",
+        "Status": "Mới 100%",
+        "Material": "Li-ion"
+    },
+    "masp": "EVE1"
+}, {
+    "name": "Pin EVE 26V",
+    "company": "EVE",
+    "img": "img/EVE26V.jpg",
+    "price": "22.000",
+    "star": 4.9,
+    "rateCount": 25872,
+    "promo": {
+        "name": "giamgia",
+        "value": "1.000"
+    },
+    "detail": {
+        "Capacity": "2600 mAh",
+        "Cycle": "800 - 1000 lần",
+        "MaxDisCharge": "10A",
+        "MaxCharge": "5A",
+        "Color": "Xanh nước biển",
+        "Size": "18 x 65 mm",
+        "Status": "Mới 100%",
+        "Material": "Li-ion"
+    },
+    "masp": "EVE2"
+}	]
