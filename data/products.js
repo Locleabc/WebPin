d205e8810d5aa6b51d61408f8f191716589b02e9
Locleabc <locleabc98@gmail.com>
2025-04-27 12:11:37 +0700

    Update Pin

--- data/products.js
+++ data/products.js
@@ -42,4 +42,48 @@ var list_products = [{
         "Material": "Li-ion"
     },
     "masp": "EVE2"
+}, {
+    "name": "Pin Batix 7.5Ah",
+    "company": "Batix",
+    "img": "img/products/75Ah.jpg",
+    "price": "450.000",
+    "star": 4.9,
+    "rateCount": 3222,
+    "promo": {
+        "name": "giamgia",
+        "value": "20.000"
+    },
+    "detail": {
+        "Capacity": "7.5 Ah",
+        "Cycle": "800 - 1000 lần",
+        "MaxDisCharge": "90A",
+        "MaxCharge": "5A",
+        "Color": "Đen",
+        "Size": "10 x 10 x 10 mm",
+        "Status": "Mới 100%, Sử dụng cho máy khoan, cắt sử dụng Pin chân phổ thông",
+        "Material": "Li-ion, Vỏ nhựa ABS + Cao su chống sốc"
+    },
+    "masp": "EVE3"
+, {
+    "name": "Pin Batix 5Ah",
+    "company": "Batix",
+    "img": "img/products/5Ah.jpg",
+    "price": "380.000",
+    "star": 4.9,
+    "rateCount": 2672,
+    "promo": {
+        "name": "giamgia",
+        "value": "20.000"
+    },
+    "detail": {
+        "Capacity": "5 Ah",
+        "Cycle": "800 - 1000 lần",
+        "MaxDisCharge": "60A",
+        "MaxCharge": "5A",
+        "Color": "Đen",
+        "Size": "10 x 10 x 10 mm",
+        "Status": "Mới 100%, Sử dụng cho máy khoan, cắt sử dụng Pin chân phổ thông",
+        "Material": "Li-ion, Vỏ nhựa ABS + Cao su chống sốc"
+    },
+    "masp": "EVE4"
 }	]
